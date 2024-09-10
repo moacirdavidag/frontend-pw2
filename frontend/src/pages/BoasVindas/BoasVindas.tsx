51ce20ccf2127809e81c0f474a9fa12a65a1f55f
moacirdavidag <moacirdavidag@gmail.com>
2024-09-10 20:00:14 -0300

    feature: Autenticação implementada (backend)

--- frontend/src/pages/BoasVindas/BoasVindas.tsx
+++ frontend/src/pages/BoasVindas/BoasVindas.tsx
@@ -2,8 +2,6 @@ import { Box, VStack, Heading, Text, Image } from "@chakra-ui/react";
 import LoginButton from "../../components/Button/LoginButton";
 import RegisterButton from "../../components/Button/RegisterButton";
 import { useNavigate } from "react-router-dom";
-import ButtonComponent from "../../components/Button/Button";
-import { MdArrowOutward } from "react-icons/md";
 
 const WelcomeScreen = () => {
   const navigate = useNavigate(); // Hook para navegação
@@ -12,7 +10,7 @@ const WelcomeScreen = () => {
   const handleLoginClick = () => {
     navigate("/login");
   };
-
+  
   return (
     <Box
       display="flex"
@@ -24,7 +22,7 @@ const WelcomeScreen = () => {
       <VStack spacing={8} maxW="400px" textAlign="center">
         {/* Logo da empresa */}
         <Image
-          src="src/assets/imgs/logo.png"
+          src="src\assets\imgs\logo.png"
           alt="Logo Delivery Acadêmico"
           mt="10px"
           width="156px"
@@ -33,7 +31,7 @@ const WelcomeScreen = () => {
 
         {/* Imagem de Boas Vindas */}
         <Image
-          src="src/assets/imgs/motoca.png"
+          src="src\assets\imgs\motoca.png"
           alt="Delivery Acadêmico"
           width="380px"
           height="280px"
@@ -41,30 +39,16 @@ const WelcomeScreen = () => {
 
         {/* Texto de Boas Vindas */}
         <Heading as="h1" fontSize="24px">
-          Bem-vindo ao Delivery Acadêmico
+          Bem vindo ao Delivery Acadêmico
         </Heading>
         <Text fontSize="16px" color="gray.600">
           Descubra uma variedade de pratos com entrega rápida.
         </Text>
 
         {/* Botão Fazer Login */}
-        {/* <LoginButton onClick={handleLoginClick} /> */}
-        <ButtonComponent
-          titulo="Fazer login"
-          background={"#e52d2d"}
-          onClick={handleLoginClick}
-          variant={"contained"}
-          icon={<MdArrowOutward />}
-        />
-
+        <LoginButton onClick={handleLoginClick} />
         {/* Botão Cadastre-se */}
-        <ButtonComponent
-          titulo="Cadastre-se"
-          variant={"outline"}
-          onClick={() => {}}
-          color={"#e52d2d"}
-          hover={{background: "green"}}
-        />
+        <RegisterButton />
       </VStack>
     </Box>
   );
