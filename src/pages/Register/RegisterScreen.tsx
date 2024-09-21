8dad0a3006144f4a1347267465b71ed7dd62b19d
Ramon <ramonoliveira_1@hotmail.comgit config --global user.email ramonoliveira_1@hotmail.comgit config --global user.email ramonoliveira_1@hotmail.com>
2024-09-21 04:43:03 -0300

    Ajustados ViewIcon em LoginForm e breakpoints da tela de Registro

--- src/pages/Register/RegisterScreen.tsx
+++ src/pages/Register/RegisterScreen.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { PageLayout } from "../../components/PageLayout/PageLayout";
-import { Header } from "../../components/Header/Header";
 import RegisterForm from "../../components/Form/RegisterForm";
 
 const RegisterScreen: React.FC = () => {
   return (
     <PageLayout>
-      <Header logoSrc="src/assets/imgs/logo.png" />
-
       <RegisterForm />
     </PageLayout>
   );
