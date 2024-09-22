2c7ac3eb105465efc86db30bcc20ee77d6768328
moacirdavidag <moacirdavidag@gmail.com>
2024-09-22 15:33:52 -0300

    view: Menu dropdown ao clicar sobre o perfil do usuário

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { HeaderWrapper } from "./style";
-import { Avatar, Box, Icon, Stack, Text } from "@chakra-ui/react";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { IoLocation } from "react-icons/io5";
+import {
+  Avatar,
+  Box,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { MdKeyboardArrowDown, MdLogout } from "react-icons/md";
+import { IoLocation, IoPerson } from "react-icons/io5";
+import theme from "../../../styles/global";
 
 const Header: React.FC = () => {
   return (
@@ -15,7 +26,21 @@ const Header: React.FC = () => {
         <Icon as={MdKeyboardArrowDown} color={"white"} w={6} h={6} />
       </Stack>
       <Box>
-        <Avatar style={{ width: "30px", height: "30px" }} />
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton isActive={isOpen}>
+                <Avatar style={{ width: "30px", height: "30px" }} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem icon={<Icon as={IoPerson} color={theme.colors.primary} />}>Perfil</MenuItem>
+                <MenuItem icon={<Icon as={MdLogout} color={theme.colors.primary} />} onClick={() => alert("Kagebunshin")}>
+                  Sair
+                </MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
       </Box>
     </HeaderWrapper>
   );
