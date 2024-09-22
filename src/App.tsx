76d45098b69ea829e02f60c4200213830f311559
Isaelson Trajano <isaelsongutembergh@gmail.com>
2024-09-22 15:32:34 -0300

    Implementando tela sobre

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import BoasVindas from "./pages/BoasVindas/BoasVindas";
 import LoginScreen from "./pages/Login/LoginScreen";
 import ForgotPasswordScreen from "./pages/ForgotPassword/ForgotPasswordScreen";
 import RegisterScreen from "./pages/Register/RegisterScreen";
+import Sobre from "./pages/Sobre/SobreScreen";
+
 
 // Componente de Boas-Vindas
 const WelcomeScreen: React.FC = () => {
@@ -22,13 +24,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<WelcomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />{" "}
+          <Route path="/login" element={<LoginScreen />} />
           <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
           <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/Sobre" element={<Sobre/>} /> 
         </Routes>
       </Router>
     </ChakraProvider>
   );
 }
-
 export default App;
